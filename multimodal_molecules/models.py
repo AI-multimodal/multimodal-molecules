ee36489b0ca4189fb160ea88af777d2341181d63
Matthew Carbone <x94carbone@gmail.com>
2022-12-22 09:35:44 -0500

    Add min/max occurence for functional groups

--- multimodal_molecules/models.py
+++ multimodal_molecules/models.py
@@ -71,6 +71,8 @@ def run_experiments(
     random_state=42,
     n_jobs=2,
     testing=False,
+    min_fg_occurrence=0.02,
+    max_fg_occurrence=0.98,
 ):
     """Summary
 
@@ -94,6 +96,12 @@ def run_experiments(
         Description
     n_jobs : int, optional
         Description
+    testing : bool, optional
+        Description
+    min_fg_occurrence : float, optional
+        Description
+    max_fg_occurrence : float, optional
+        Description
     """
 
     # "Sort" the conditions
@@ -114,7 +122,17 @@ def run_experiments(
 
     print(f"Total XANES data has shape {xanes_data.shape}")
     L = len(data["FG"])
+    print(f"Total of {L} functional groups")
     for ii, (key, binary_targets) in enumerate(data["FG"].items()):
+
+        # Check that the occurence of the functional groups falls into the
+        # specified sweet spot
+        total_targets = len(binary_targets)
+        targets_on = binary_targets.sum()
+        ratio = targets_on / total_targets
+        if not (min_fg_occurrence < ratio < max_fg_occurrence):
+            print(f"{key} has occurence of {ratio:.02f} - continuing")
+            continue
 
         # Get the name of the model to be trained. This should correspond to
         # a set of conditions (XANES, kept data, etc.) and a functional group
@@ -140,7 +158,7 @@ def run_experiments(
             model = RandomForestClassifier(
                 n_jobs=n_jobs, random_state=random_state, verbose=True
             )
-            model.fit(x_train, y_train, n_jobs=n_jobs)
+            model.fit(x_train, y_train)
 
             # Run the model report and save the it as a json file
             report = Report()
