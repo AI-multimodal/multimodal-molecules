c4dd7eaf4b56ac4564b11e773fb7eb16c14e0b18
Matthew Carbone <x94carbone@gmail.com>
2022-12-24 00:15:02 -0500

    Update run_models

--- run_models.py
+++ run_models.py
@@ -1,10 +1,14 @@
 from multimodal_molecules.models import Results
 
-OUTPUT_DIR = "results/221222"
+OUTPUT_DIR = "results/221223"
+INPUT_DIR = "data/221205"
 CONDITIONS = [
-    "O-XANES",
+    "C-XANES",
     "N-XANES",
-    "O-XANES,N-XANES",
+    "O-XANES",
+    "C-XANES,N-XANES",
+    "C-XANES,O-XANES",
+    "C-XANES,N-XANES,O-XANES"
 ]
 
 if __name__ == "__main__":
@@ -15,9 +19,9 @@ if __name__ == "__main__":
             index_data_name="221205_index.csv",
         )
         results.run_experiments(
-            input_data_directory="data/221205",
-            output_data_directory="results/221222",
-            n_jobs=8,
+            input_data_directory=INPUT_DIR,
+            output_data_directory=OUTPUT_DIR,
+            n_jobs=4,
             debug=-1,
             compute_feature_importance=True,
         )
