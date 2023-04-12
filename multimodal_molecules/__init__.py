4d71dd9edb516936bf5a364cd2e715424bfc57f3
Matthew Carbone <x94carbone@gmail.com>
2023-04-12 09:58:00 -0400

    Bugfix init

--- multimodal_molecules/__init__.py
+++ multimodal_molecules/__init__.py
@@ -1,5 +1,8 @@
 from dunamai import Version
 
-version = Version.from_any_vcs()
-__version__ = version.serialize()
-del version
+try:
+    version = Version.from_any_vcs()
+    __version__ = version.serialize()
+    del version
+except RuntimeError:
+    __version__ = None
