8088b61f319ac54ce9bbc05efed8bc2d9bf094f6
Matthew Carbone <x94carbone@gmail.com>
2024-01-18 22:16:08 +0100

    Update old files, add new plotting

--- multimodal_molecules/plotting.py
+++ multimodal_molecules/plotting.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np
+from scipy.interpolate import interpn
 
 
 def set_defaults(labelsize=12, dpi=250):
@@ -21,7 +23,6 @@ def set_grids(
     right=True,
     top=True,
 ):
-
     if minorticks:
         ax.minorticks_on()
 
@@ -37,3 +38,42 @@ def set_grids(
     if grid:
         ax.grid(which="minor", alpha=0.2, linestyle=":")
         ax.grid(which="major", alpha=0.5)
+
+
+# https://stackoverflow.com/questions/20105364/how-can-i-make-a-scatter-plot-colored-by-density-in-matplotlib
+def density_scatter(x, y, ax, sort=True, bins=20, **kwargs):
+    """
+    Scatter plot colored by 2d histogram
+    """
+
+    data, x_e, y_e = np.histogram2d(x, y, bins=bins, density=True)
+    z = interpn(
+        (0.5 * (x_e[1:] + x_e[:-1]), 0.5 * (y_e[1:] + y_e[:-1])),
+        data,
+        np.vstack([x, y]).T,
+        method="splinef2d",
+        bounds_error=False,
+    )
+
+    # To be sure to plot all data
+    z[np.where(np.isnan(z))] = 0.0
+
+    # Sort the points by density, so that the densest points are plotted last
+    if sort:
+        idx = z.argsort()
+        x, y, z = x[idx], y[idx], z[idx]
+
+    scat = ax.scatter(x, y, c=z, **kwargs)
+    return scat
+
+
+def remove_axis_spines(ax, visible=True):
+    # ax.spines["right"].set_visible(False)
+    # ax.spines["top"].set_visible(False)
+    for pos in ["left", "right", "top", "bottom"]:
+        ax.spines[pos].set_linewidth(0.5)
+        if not visible:
+            ax.spines[pos].set_visible(False)
+    ax.tick_params(left=False, bottom=False)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
